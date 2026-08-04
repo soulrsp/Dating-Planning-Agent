d72d7f9a6b6293992a540f52f27ba9ecb580058f
soulrsp <soulrsp@gmail.com>
2026-08-04 15:34:57 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aura-date-planner-v20260802_v338';
+const CACHE_NAME = 'aura-date-planner-v20260802_v339';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
